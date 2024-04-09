6c51517a78f55f6be122b2ab4c7a9a70464ffb65
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2024-04-09 13:38:25 +0600

    using ui

---
+++ FrontEnd/Models/GameList.cs
@@ -0,0 +1,14 @@
+﻿namespace FrontEnd.Models;
+
+public class GameList
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Genre { get; set; }
+
+    public decimal Price { get; set; }
+
+    public DateOnly ReleaseDate { get; set; }
+}
