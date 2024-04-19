f048c21b5a3e533c2f2009f877c7afa6e93abf27
Faisal Alam Shiblu <45782662+faisalShiblu@users.noreply.github.com>
2024-04-19 17:04:00 +0600

    form

--- FrontEnd/Clients/GamesClient.cs
+++ FrontEnd/Clients/GamesClient.cs
@@ -8,11 +8,28 @@ public class GamesClient()
     //  => await httpClient.GetFromJsonAsync<GameList[]>("games") ?? [];
 
     private readonly List<GameList> games = [
-    new (){Id = 1, Name = "One", Genre="Action", Price = 10.88M, ReleaseDate = new DateOnly(1999,4,10)},
-    new (){Id = 2, Name = "One-2", Genre="Action", Price = 100.98M, ReleaseDate = new DateOnly(1999,4,10)},
-    new (){Id = 3, Name = "One-3", Genre="Action", Price = 10M, ReleaseDate = new DateOnly(1999,4,10)},
-    new (){Id = 4, Name = "One-4", Genre="Action", Price = 15.88M, ReleaseDate = new DateOnly(1999,4,10)}
+    new (){Id = 1, Name = "One", Genre="A Genre", Price = 10.88M, ReleaseDate = new DateOnly(1999,4,10)},
+    new (){Id = 2, Name = "One-2", Genre="D Genre", Price = 100.98M, ReleaseDate = new DateOnly(1999,4,10)},
+    new (){Id = 3, Name = "One-3", Genre="C Genre", Price = 10M, ReleaseDate = new DateOnly(1999,4,10)},
+    new (){Id = 4, Name = "One-4", Genre="A Genre", Price = 15.88M, ReleaseDate = new DateOnly(1999,4,10)}
       ];
 
+    private readonly Genre[] genres = new GenresClient().GetGenres();
     public GameList[] GetGames() => [.. games];
+
+    public void AddGame(GameDetails game)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
+        var genre = genres.Single(s => s.Id == int.Parse(game.GenreId));
+        var gList = new GameList
+        {
+            Id = games.Count + 1,
+            Name = game.Name,
+            Genre = genre.Name,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate,
+        };
+
+        games.Add(gList);
+    }
 }
---
+++ FrontEnd/Clients/GenresClient.cs
@@ -0,0 +1,15 @@
+﻿using FrontEnd.Models;
+
+namespace FrontEnd.Clients;
+
+public class GenresClient
+{
+    private readonly Genre[] genres = [
+        new (){Id = 1, Name="A Genre"},
+        new (){Id = 2, Name="B Genre"},
+        new (){Id = 3, Name="C Genre"},
+        new (){Id = 4, Name="D Genre"},
+        ];
+
+    public Genre[] GetGenres() => genres;
+}
